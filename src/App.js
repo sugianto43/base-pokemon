ad62daa038a1f4e247eea255711c64294b39381b
sugianto43 <antho9922@gmail.com>
2021-05-26 11:09:51 +0800

    initial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+const GRAPHQL_POKEMON = "https://graphql-pokemon2.vercel.app";
+const POKEMONS_QUERY = `
+  {
+    pokemons(first: 300) {
+      id
+      name
+      image
+    }
+  }
+`;
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.post(GRAPHQL_POKEMON, {
+      query: POKEMONS_QUERY,
+    })
+    .then(res=>{
+      console.log(res.data.data)
+      setData(res.data.data.pokemons)
+    });
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data?.map((item) => (
+        <img src={item.image} alt={item.name} />
+      ))}
     </div>
   );
 }
